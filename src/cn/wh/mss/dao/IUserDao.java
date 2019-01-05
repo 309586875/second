a230615830f2e6edee0476aeb73bdaf035988254
309586875 <zheshiwodemima123>
2019-01-05 16:26:52 +0800

    123

--- src/cn/wh/mss/dao/IUserDao.java
+++ src/cn/wh/mss/dao/IUserDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import cn.wh.mss.doamin.User;
 
 public interface IUserDao {
-	
+	//132
 	public int save(User u);
 	public List<User> query();
 	public boolean checkThis(String username, String password);
